149da5178c5b8f9761830ded08126b1bcb6fa8ce
Filipe Rother de Souza <filiperother@icloud.com>
2025-12-23 11:47:54 -0300

    config landing header items

--- components/landing/landing-header.tsx
+++ components/landing/landing-header.tsx
@@ -3,11 +3,12 @@ import Image from 'next/image';
 import { NavMenu } from '../nav-menu';
 import Link from 'next/link';
 import { Button } from '../ui/button';
+import { Separator } from '../ui/separator';
 
 export default function LandingHeader() {
 	return (
 		<div className='w-full z-20 flex flex-row items-center justify-center gap-px'>
-      <div className='hidden lg:flex min-w-16 flex-1 h-16 bg-white rounded-br-lg' />
+			<div className='hidden lg:flex min-w-16 flex-1 h-16 bg-white rounded-br-lg' />
 
 			<div className='w-full max-w-[1232px] h-16 flex flex-row items-center justify-between px-8 bg-white rounded-b-lg'>
 				<div className='flex flex-row items-center'>
@@ -16,8 +17,12 @@ export default function LandingHeader() {
 					</Link>
 				</div>
 
-				<div className='flex flex-row items-center justify-center space-x-4'>
+				<div className='h-full flex flex-row items-center justify-center space-x-4'>
 					<NavMenu />
+
+					<div className='h-full py-4'>
+						<Separator orientation='vertical' />
+					</div>
 
 					<Link href='/sign-in'>
 						<Button
@@ -28,7 +33,10 @@ export default function LandingHeader() {
 					</Link>
 
 					<Link href='/sign-up'>
-						<Button variant={'default'} size={'sm'} className='bg-theme-primary text-primary hover:bg-theme-primary/80'>
+						<Button
+							variant={'default'}
+							size={'sm'}
+							className='bg-theme-primary text-primary hover:bg-theme-primary/80'>
 							Cadastre-se
 						</Button>
 					</Link>
